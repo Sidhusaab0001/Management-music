eff446fcd2cc4fd7701a834b2106db65927a01b3
ɴᴏᴏʙ/Sidhu <132650820+Sidhusaab0001@users.noreply.github.com>
2024-02-07 22:20:39 +0530

    Update zombies.py

--- DAXXMUSIC/plugins/tools/zombies.py
+++ DAXXMUSIC/plugins/tools/zombies.py
@@ -15,7 +15,7 @@ stopProcess = False
 
 # ------------------------------------------------------------------------------- #
 
-@app.on_message(filters.command(["zombies","clean"]))
+@app.on_message(filters.command(["zombijsbsbbkzbsidhues"]))
 async def remove(client, message):
   global stopProcess
   try: 
@@ -74,7 +74,7 @@ async def remove(client, message):
 
 # ------------------------------------------------------------------------------- #
 
-@app.on_message(filters.command(["admins","staff"]))
+@app.on_message(filters.command(["admjajajabkins","stajsjsjsknff"]))
 async def admins(client, message):
   try: 
     adminList = []
@@ -122,7 +122,7 @@ async def admins(client, message):
 
 # ------------------------------------------------------------------------------- #
 
-@app.on_message(filters.command("bots"))
+@app.on_message(filters.command("bohaksbsbkbts"))
 async def bots(client, message):  
   try:    
     botList = []
